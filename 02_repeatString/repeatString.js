9cdf5740f7f7a01b0c2610d06c67200ea12599a5
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-02 10:01:40 -0400

    Solns for JS Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str,n) {
+  let newString = "";
 
+  for(let i=0; i < n; i++){
+    newString = newString + str;
+  }
+
+  if(n >= 0){
+    return newString;
+  } else {
+    return "ERROR";
+  }
+  
 };
 
 // Do not edit below this line
