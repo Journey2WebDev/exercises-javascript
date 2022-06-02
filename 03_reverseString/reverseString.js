c05feb4ed4ec0ca07246b3d12169e111cfa9a789
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-02 10:29:10 -0400

    Solns to JS Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(xstr) {
+  let newString = "";
 
+  for(let c=(xstr.length - 1); c >= 0; c--){
+    newString = newString + xstr[c];
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
