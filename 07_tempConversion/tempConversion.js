c1fadf3a981f0ff6b4ab054724434f88d60a810c
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-02 16:08:46 -0400

    Solns to JS exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  // Convert fahrenheit to celsius
+  let ctemp = ((ftemp - 32)*5)/9;
+  // If result isn't a whole number (ie, a decimal exists), round to 1 decimal precision
+  if(ctemp.toString().includes(".")){
+    ctemp = parseFloat(ctemp.toFixed(1));
+  }
+  
+  return ctemp;
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  // Convert celsius to fahrenheit
+  let ftemp = ctemp*(9/5) + 32;
+  // If result isn't a whole number (ie, a decimal exists), round to 1 decimal precision
+  if(ftemp.toString().includes(".")){
+    ftemp = parseFloat(ftemp.toFixed(1));
+  }
+  
+  return ftemp;
 };
 
 // Do not edit below this line
