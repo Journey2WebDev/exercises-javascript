d77265efdeddcccb4021c595e021c77d5595d211
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-02 15:40:49 -0400

    Solns for JS exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(num) {
+  
+  let isLeapYear = false;
 
+  if(num % 400 == 0){
+    isLeapYear = true;
+  } else if (num % 4 == 0 && num % 100 != 0){
+    isLeapYear = true;
+  } else {
+    isLeapYear = false;
+  }
+  return isLeapYear;
 };
 
 // Do not edit below this line
