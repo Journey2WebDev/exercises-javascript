18b592c3adc0e61e7f23e21c644cde6eb03b30f6
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-02 14:11:14 -0400

    Use rest Parameter syntax for undetermined number of fx args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,34 +1,15 @@
-const removeFromArray = function(xarray, val1, val2, val3, val4) { 
-  // Create array for 'check' values (ie, are these values in xarray)
-  let checkArray = [];
-
-  if(typeof val1 != "undefined"){
-    checkArray.push(val1);
-  }
-  if (typeof val2 != "undefined"){
-    checkArray.push(val2);
-  }
-  if (typeof val3 != "undefined"){
-    checkArray.push(val3);
-  }
-  if (typeof val4 != "undefined"){
-    checkArray.push(val4);
-  }
-
-  // Nested loop to evaluate checkArray elements against each xarray element
-  for(let v=0; v < checkArray.length; v++){
-
-    for(let i=0; i < xarray.length; i++){
-      checkResult = xarray[i] === checkArray[v];
-    
-      // If checkResult is TRUE, remove from xarray using splice()
+const removeFromArray = function(inputArray, ...checkVals) { 
+  // Nested loop evaluates each element of checkVals against each element of inputArray
+  for(let v=0; v < checkVals.length; v++){
+    for(let i=0; i < inputArray.length; i++){
+      checkResult = inputArray[i] === checkVals[v];
+      // If checkResult is TRUE, remove from inputArray using splice()
       if(checkResult){
-        xarray.splice(i,1);
+        inputArray.splice(i,1);
       }
     }
-
   }
-  return xarray;
+  return inputArray;
 };
 
 // Do not edit below this line
