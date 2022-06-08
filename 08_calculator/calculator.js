4b1585bd7322a1fd64dca2debfa143998eba05fa
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-08 19:32:55 -0400

    Solns to JS exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a,b) {
+  if(a>=0 && b>=0){
+    return a + b;
+  }
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(ar) {
+	total = 0;
+  ar.forEach(e => {total += e});
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(ar) {
+  total = 1;
+  ar.forEach(e => total *= e);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+  let factNums = [];
+  for(i=1; i <= num; i++){
+    factNums[i] = i;
+  }
+
+  total = 1;
+  factNums.forEach(e => total *= e);
+
+  if(num == 0){
+    return 1
+  } else {
+    return total;  
+  }
 };
 
 // Do not edit below this line
