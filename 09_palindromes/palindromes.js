e5eb777c443a58820cc93e9971d6c2ad1f0fbcbe
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-08 23:40:48 -0400

    Solns to JS exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (text) {
+
+  let text_forward = "";
+  let text_reverse = "";
+  
+  for(i=0; i<text.length; i++){
+    let letter = text.substring(i,i+1);
+
+    if(letter.match(/[a-z]/i)){
+      text_reverse = letter + text_reverse;
+      text_forward = text_forward + letter;
+    }
+  }
+  
+  if (text_forward.toUpperCase() === text_reverse.toUpperCase()){
+    return true;
+  } else {
+    return false;
+  }
 
 };
 
