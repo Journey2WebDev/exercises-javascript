89df9ae135189a65da6eb993aaf68f51f306ae86
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-09 00:14:53 -0400

    Solns to JS exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array_in) {
+  let array_out = [];
+  array_in.forEach(e => array_out.push(e.title));
+  return array_out;
 };
 
 // Do not edit below this line
