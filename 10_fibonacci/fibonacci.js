9388d3e8151c31fbc3c28a5dd131015b17ce18ce
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-09 00:00:55 -0400

    Solns to JS exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(seq_num) {
+
+  let fib = [1, 1];
+
+  if(seq_num <= 0) {
+    return "OOPS";
+  } else if(typeof seq_num == "string"){
+    seq_num = parseInt(seq_num);
+  }
+
+  for(n=1; n<=(seq_num-2); n++){
+    fib_next = fib[(fib.length - 1)] + fib[(fib.length - 2)];
+    fib.push(fib_next);
+  }
+
+  return fib.pop();
 
 };
 
