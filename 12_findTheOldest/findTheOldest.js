fda96ada560d1fe983367187f23797f21497b020
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-09 08:59:35 -0400

    Solns for JS exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
 
+  function ageProperty(o){
+    let todayYear = new Date().getFullYear();
+  
+    if(!o.hasOwnProperty("yearOfDeath")){
+      o["yearOfDeath"] = todayYear;
+    }
+  
+    o["age"] = o.yearOfDeath - o.yearOfBirth;
+    return o;
+  };
+
+  const temp = obj.map(ageProperty);
+  const temp2 = obj.sort((a,b) => a.age < b.age ? 1 : -1);
+  
+  let oldest = temp2[0];
+  return oldest;
 };
 
 // Do not edit below this line
